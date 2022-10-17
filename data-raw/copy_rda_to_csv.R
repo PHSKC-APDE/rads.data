a53c92495407c5cf63d298d65b51c09282ee9c31
Danny Colombara <48296707+dcolombara@users.noreply.github.com>
2022-10-17 07:10:37 -0700

    added code to batch convert CSV to RDA

--- data-raw/copy_rda_to_csv.R
+++ data-raw/copy_rda_to_csv.R
@@ -4,6 +4,7 @@
 # of folder in /inst/extdata. E.g., spatial_zip_hca.rda >>
 # inst/extdata/spatial_data/zip_hca.csv
 
+devtools::source_url("https://raw.githubusercontent.com/PHSKC-APDE/rads/main/R/utilities.R") # get sql_clean
 library(data.table)
 
 # convert .rda to .csv and move to /inst/extdata
@@ -15,6 +16,19 @@ library(data.table)
       write.csv(get(data(list = rdaname)), file = paste0("inst/extdata/", stem, "_data/", gsub("rda$", "csv", gsub(paste0(stem, "_"), "", i, ignore.case = T), ignore.case = T)), row.names = F)
       rm(list=rdaname)
     }
+
+
+# convert .csv to .rda (if beginning with *.csv in /inst/extdata)
+  stem <- 'population-OR-spatial-OR-occupation-OR-icd'
+  fn <- list.files(paste0("inst/extdata/", stem, "_data/"), pattern = "csv$", ignore.case = T)
+  for(i in fn){
+    rdaname <- paste0(stem, "_", gsub("\\.csv", "", i, ignore.case = T))
+    assign(rdaname, data.table::fread(paste0("inst/extdata/", stem, "_data/", i)) )
+    sql_clean(get(rdaname))
+    save(list = rdaname, file = paste0("data/", rdaname, ".rda") , compress = "bzip2", version = 3)
+    rm(rdaname)
+  }
+
 
 # convert .csv to .rda (if beginning with *.csv in /data)
   # fn <- list.files("data", pattern = "csv$", ignore.case = T)
