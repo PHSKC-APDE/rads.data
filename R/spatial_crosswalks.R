a102073f1401923a70b748b626da622b2760c48f
Danny Colombara <dcolombara@kingcounty.gov>
2025-07-31 15:52:43 -0700

    updated to markdown & fixed typos

--- R/spatial_crosswalks.R
+++ R/spatial_crosswalks.R
@@ -1,17 +1,16 @@
 # spatial_ids_and_names ----
-#' Crosswalk between geo_id and geo_names for an assortment of gegraphies
+#' Crosswalk between geo_id and geo_names for an assortment of geographies
 #'
 #' @description
-#' Mostly a helper file for rads::get_population
-#'
+#' Mostly a helper file for [rads::get_population()]
 #'
 #' @format A data.table with 506 rows and 6 variables:
 #'
-#'   \code{geo_id_code}: Numeric code of the geography,
+#'   `geo_id_code`: Numeric code of the geography,
 #'
-#'   \code{geo_id}: Name of the geography,
+#'   `geo_id`: Name of the geography,
 #'
-#'   \code{type}: Geography type
+#'   `type`: Geography type
 #'
 #' @name spatial_ids_and_names
 "spatial_ids_and_names"
